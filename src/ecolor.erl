42486cf41b72211ac2cfa09b32469f5bd3e7de45
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2021-08-26 16:08:52 +0700

    join several SGR attributes

--- src/ecolor.erl
+++ src/ecolor.erl
@@ -1,3 +1,53 @@
 -module(ecolor).
 
 -export([]).
+
+-include("ecolor.hrl").
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+%%
+%% TYPES.
+%%
+
+-type sgr_attribute() :: binary().
+
+%%
+%% HELPERS.
+%%
+
+%% Joins several Select Graphic Rendition (SGR) attributes together
+%% (with semicolon).
+-spec join_attributes([sgr_attribute()]) -> sgr_attribute().
+join_attributes(Attributes) ->
+    R = fun(Elem, <<>>) ->
+                Elem;
+           (<<>>, Acc)  ->
+                Acc;
+           (Elem, Acc) ->
+                <<Acc/binary, ?ATTRIBUTE_SEPARATOR/binary, Elem/binary>>
+        end,
+    lists:foldl(R, <<>>, Attributes).
+
+%%
+%% TESTS.
+%%
+
+-ifdef(TEST).
+
+join_attributes_test_() ->
+    Tests = [
+             {[], <<>>},
+             {[<<"a">>], <<"a">>},
+             {[<<"a">>, <<"b">>], <<"a;b">>},
+             {[<<"a">>, <<"b">>, <<"c">>], <<"a;b;c">>},
+             {[<<"1">>, <<"31">>], <<"1;31">>},
+             {[<<"1">>, <<"37">>, <<"41">>], <<"1;37;41">>},
+             {[<<>>, <<"37">>, <<"41">>], <<"37;41">>},
+             {[<<>>, <<"37">>, <<>>], <<"37">>}
+            ],
+    [?_assertEqual(E, join_attributes(I)) || {I, E} <- Tests].
+
+-endif.
